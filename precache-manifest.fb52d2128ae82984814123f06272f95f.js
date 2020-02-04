2d497e2022136fd32a2eef7e10b009517da9ef50
AlexAtNet <alex.netkachov@gmail.com>
2020-02-04 17:21:42 +0000

    Updates

---
+++ precache-manifest.fb52d2128ae82984814123f06272f95f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf68747dd493bad429ac6a5ce75d600",
+    "url": "/modelx/index.html"
+  },
+  {
+    "revision": "300f775429aab9957aea",
+    "url": "/modelx/static/css/main.cdd2a04e.chunk.css"
+  },
+  {
+    "revision": "1453f0e81236b9c72ca4",
+    "url": "/modelx/static/js/2.cb8c64e3.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/modelx/static/js/2.cb8c64e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "300f775429aab9957aea",
+    "url": "/modelx/static/js/main.8f70a991.chunk.js"
+  },
+  {
+    "revision": "1921f15ac2d753350ddd",
+    "url": "/modelx/static/js/runtime-main.b963407d.js"
+  }
+]);
